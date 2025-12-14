b0f19cadc4a93a36a8a2d9f94b9766281e322680
Hayeon Baek <bhy0512@gmail.com>
2025-12-14 16:50:44 +0900

    feat: erroHandler에서 기본 에러 객체를 반환하도록 수정

--- src/shared/lib/error-handler.ts
+++ src/shared/lib/error-handler.ts
@@ -1,8 +1,15 @@
 import { isAxiosError, type AxiosError } from 'axios';
+import type { BaseResponse } from '../types/auth.type';
 
-export const errorHandler = (error: AxiosError | unknown) => {
+export const errorHandler = (error: AxiosError | unknown): BaseResponse => {
   console.error('에러 발생:', error);
+
   if (isAxiosError(error) && error.response?.data) {
     return error.response.data;
   }
+
+  return {
+    success: false,
+    message: '알 수 없는 오류가 발생했습니다. 잠시 후 다시 시도해 주세요.',
+  };
 };
