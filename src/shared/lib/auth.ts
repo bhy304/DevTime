a45b61ac31d5a5df22c5948c10dd01d6f6aea203
Hayeon Baek <bhy0512@gmail.com>
2025-12-19 01:43:30 +0900

    feat: 인증 토큰 관리 로직 개선 및 유틸리티 함수 추가

--- src/shared/lib/auth.ts
+++ src/shared/lib/auth.ts
@@ -1,4 +1,18 @@
+const AUTH_KEY = {
+  ACCESS_TOKEN: "accessToken",
+  REFRESH_TOKEN: "refreshToken",
+};
+
 export const setTokens = (accessToken: string, refreshToken: string) => {
-  localStorage.setItem('accessToken', accessToken);
-  localStorage.setItem('refreshToken', refreshToken);
+  localStorage.setItem(AUTH_KEY.ACCESS_TOKEN, accessToken);
+  localStorage.setItem(AUTH_KEY.REFRESH_TOKEN, refreshToken);
+};
+
+export const getToken = (token: string) => {
+  return localStorage.getItem(token);
+};
+
+export const removeTokens = () => {
+  localStorage.removeItem(AUTH_KEY.ACCESS_TOKEN);
+  localStorage.removeItem(AUTH_KEY.REFRESH_TOKEN);
 };
